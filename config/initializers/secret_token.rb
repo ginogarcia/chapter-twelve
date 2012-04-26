e5ed3932ab883a573cac5e35db8a263a2b09b7fd
Gino Garcia <ginogarcia@netzero.net>
2012-04-26 00:36:17 -0500

    pulled chapter 12

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChapterTwelve::Application.config.secret_token = 'cd42dd5bfee2894cb016759ce3bc2fe2fbcf4e8d6f9aa5c8d6da0e8792470dfe135ee6f6710b510d65bba1a87b9353157edda636a05c1350eacdcf7dd3e0beef'
