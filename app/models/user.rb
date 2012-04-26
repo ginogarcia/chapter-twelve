a4e6b119d9b3f7bbbdd5c82fa6b206ac8fcbdc3a
Gino Garcia <ginogarcia@netzero.net>
2012-04-26 16:22:40 -0500

    video time 37:03 working

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,20 @@
 class User < ActiveRecord::Base
 
     attr_accessor :password
-    attr_accessible :name, :email, :password , :password_confirmation
+    attr_accessible :name, :email,    :password , :password_confirmation
     
-    has_many :microposts,    :dependent => :destroy
-    has_many :relationships, :dependent => :destroy,
-                             :foreign_key => "follower_id"
-    has_many :following, :through => :relationships, :source => :followed
+    has_many :microposts,             :dependent => :destroy
+    has_many :relationships,          :dependent => :destroy,
+                                      :foreign_key => "follower_id"
+    has_many :reverse_relationships,  :dependent => :destroy,
+                                      :foreign_key => "followed_id",
+                                      :class_name => "Relationship"
+        
+    has_many :following, :through =>  :relationships, :source => :followed
+    has_many :followers, :through =>  :reverse_relationships,
+                                      :source => :follower
+    
+    
         
                    
    # email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #should work but it is failing in the begining and ending of the expression \A.....\z
